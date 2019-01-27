a104301543c8fa58e911d5b9491a38e04c0b7add
OstapHetman <prickly4528@gmail.com>
2019-01-27 11:46:02 +0200

    logging users in

--- scripts/auth.js
+++ scripts/auth.js
@@ -25,3 +25,22 @@ logout.addEventListener("click", e => {
     console.log("User signed out");
   });
 });
+
+// loging
+const loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  // sign up the user
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
